bb6655f68c98790c773b940d55134997e4e0719e
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-04 13:06:07 +0600

    second reason added

--- undefiend.js
+++ undefiend.js
@@ -2,9 +2,17 @@
 /*
 8 ways to get undefiend
 
-1.variable that is not intialized
+1.variable that is not initialized
+2.function with no return
+
 
 */ 
 
 let first;
 console.log(first)
+
+function second(a, b){
+    const total = a + b
+}
+const result = second()
+console.log(result)
