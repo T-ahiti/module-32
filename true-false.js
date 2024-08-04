2e40716a17d403f37907709e29fd3f4070530a02
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-04 21:17:17 +0600

    string rule added

--- true-false.js
+++ true-false.js
@@ -1,11 +1,15 @@
 
 /*
 Truthy:
-1.any number +/- will be truthy except 0
+1.true
+2.any number +/- will be truthy except 0
+3.any string except emty string
+
 
 Falsy:
 1. false
 2.0
+3. '' emty string
 */ 
 const x1 = false;
 if(x1){
@@ -30,3 +34,21 @@ if(x22){
 else{
     console.log('value of x is falsy')
 }
+
+const x31 = 'Salman khan';
+if(x31){
+    console.log('value of x is truthy')
+}
+else{
+    console.log('value of x is falsy')
+}
+
+// empty string er vitore ekta space thakleo truth hobe
+const x32 = '';
+if(x32){
+    console.log('value of x is truthy')
+}
+else{
+    console.log('value of x is falsy')
+}
+
