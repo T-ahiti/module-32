31f7194ac1a9773030af16df82ba3628f2177571
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-04 17:49:40 +0600

    7th reason added

--- undefiend.js
+++ undefiend.js
@@ -6,6 +6,10 @@
 2.function with no return
 3. parameter that is not passes will be undefiened 
 4.if you don't return anything. just return likhe boshaye rakhle undefiened paba
+5.property that doesn't exists will give you undefiend
+6.accessing array elemetns outside of the index range
+7.deleting an element inside an array
+8.set a value directly to undefiiend
 
 */ 
 
@@ -43,3 +47,17 @@ console.log(total)
 
 const fifth = {id:2, name:'ponchom', age:30}
 console.log(fifth.salary)
+
+
+
+const sixth = [3, 23, 3, 54, 23, 76, 54]
+console.log(sixth[200])
+
+
+
+const seven = [3, 23, 3, 54, 23, 76, 54]
+delete seven[1]
+console.log(seven)
+
+
+
