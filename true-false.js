4e7f0634d4257bfa8140d5074f24f501f890d6a6
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-04 21:12:56 +0600

    two rules added

--- true-false.js
+++ true-false.js
@@ -1,2 +1,32 @@
 
+/*
+Truthy:
+1.any number +/- will be truthy except 0
 
+Falsy:
+1. false
+2.0
+*/ 
+const x1 = false;
+if(x1){
+    console.log('value of x is truthy')
+}
+else{
+    console.log('value of x is falsy')
+}
+
+const x21 = 4;
+if(x21){
+    console.log('value of x is truthy')
+}
+else{
+    console.log('value of x is falsy')
+}
+
+const x22 = 0;
+if(x22){
+    console.log('value of x is truthy')
+}
+else{
+    console.log('value of x is falsy')
+}
