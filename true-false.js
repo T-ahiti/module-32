0affb3de2f3fdd192f1b4d0143330f3d330c2c77
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-04 21:21:28 +0600

    null and undefiend rule added

--- true-false.js
+++ true-false.js
@@ -10,6 +10,8 @@ Falsy:
 1. false
 2.0
 3. '' emty string
+4.undefiend 
+5. null
 */ 
 const x1 = false;
 if(x1){
@@ -52,3 +54,22 @@ else{
     console.log('value of x is falsy')
 }
 
+
+let x4;
+console.log(x4)
+if(x4){
+    console.log('value of x is truthy')
+}
+else{
+    console.log('value of x is falsy')
+}
+
+
+let x5 = null;
+console.log(x5)
+if(x5){
+    console.log('value of x is truthy')
+}
+else{
+    console.log('value of x is falsy')
+}
