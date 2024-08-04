9abb58ee90ed151cea73236595c92eef2f020f44
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-04 14:15:12 +0600

    4th reason added

--- undefiend.js
+++ undefiend.js
@@ -5,11 +5,14 @@
 1.variable that is not initialized
 2.function with no return
 3. parameter that is not passes will be undefiened 
+4.if you don't return anything. just return likhe boshaye rakhle undefiened paba
 
 */ 
 
 let first;
 console.log(first)
+
+
 
 function second(a, b){
     const total = a + b
@@ -17,9 +20,21 @@ function second(a, b){
 const result = second()
 console.log(result)
 
+
+
 function third(a, b, c, d){
     const total = a + b + c + d
     console.log(a,b,c,d)
 }
-
 third(2,5)
+
+
+
+function noNegative(a, b){
+    if(a < 0 || b < 0){
+        return
+    }
+    return a + b
+}
+const total = noNegative(2, -6)
+console.log(total)
