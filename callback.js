5a1a01ccb01bf7c5dbe55fd26312cddde27c4c13
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-08 00:57:19 +0600

    function event added

--- callback.js
+++ callback.js
@@ -1,7 +1,19 @@
 
-function greeting(person){
-    console.log(person)
+function greeting(greetingHandler, name){
+    greetingHandler(name)
+}
+// const object = {price:45000, brand:'lenovo', memory:'8gb'}
+// greeting(object)
+
+function greetingHandler(name){
+    console.log('good morning', name)
 }
 
-const object = {price:45000, brand:'lenovo', memory:'8gb'}
-greeting(object)
+function greetEvening(name){
+    console.log('good evening', name)
+}
+
+greeting(greetingHandler, 'tom hanks')
+greeting(greetingHandler, 'tom cruise')
+greeting(greetingHandler, 'tom horse')
+greeting(greetEvening, 'tom bon')
