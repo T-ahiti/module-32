17c9156d0b845666f84db17a0ba6a2b8a30afac8
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-07 21:18:37 +0600

    primi and non primi added

--- triple.js
+++ triple.js
@@ -1,10 +1,24 @@
 
 const first = 3
-const second = '3'
-// === dile type check kore.. == dile just man check kore
+const second = 3
+// === dile type and man check kore.. == dile just man check kore                                                                                                             
 if(first == second){
     console.log('values are equal')
 }
 else{
     console.log('values are not equal')
 }
+
+const first1 = 1
+const second2 = true
+// === dile type check kore.. == dile just man check kore                                                                                                             
+if(first1 == second2){
+    console.log('values are equal')
+}
+else{
+    console.log('values are not equal')
+}
+
+// type corecrion
+// type conversion
+// number boolean ke equal bolte pare but array or object compare korte pare na always not equal bolbe
