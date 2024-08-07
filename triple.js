82d4f3358979e6c668406c9118d0fb5495445512
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-07 19:27:34 +0600

    type checked

---
+++ triple.js
@@ -0,0 +1,10 @@
+
+const first = 3
+const second = '3'
+// === dile type check kore.. == dile just man check kore
+if(first == second){
+    console.log('values are equal')
+}
+else{
+    console.log('values are not equal')
+}
