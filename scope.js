1fb33f4655720637c1b6f57dec467904a2241b73
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-07 21:30:50 +0600

    two scope added

--- scope.js
+++ scope.js
@@ -2,7 +2,13 @@
 function add ( a, b){
   const total = a + b
   console.log(a, b)
+  if(b>5){
+    const sum = 25 + a + b
+    console.log(sum)
+  }
+//   console.log(sum)
   return total
 }
 
+//  console.log(a, b)
 add (4, 7)
