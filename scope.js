684bff7043a5e46f729ea48e8dabebf78ae3649a
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-07 22:40:14 +0600

    second commit

--- scope.js
+++ scope.js
@@ -6,9 +6,18 @@ function add ( a, b){
     const sum = 25 + a + b
     console.log(sum)
   }
+  else{
+    const sum = 5 + a + b
+    var current = sum
+  }
+  console.log(current)
+  return total
 //   console.log(sum)
   return total
 }
 
 //  console.log(a, b)
 add (4, 7)
+
+// var scope mane na o scope er baire jeye upore uthaye d3ey jake hoisting bole \
+// let use kora valo 
