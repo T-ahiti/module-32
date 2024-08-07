e8de50860c228a0b7b71e33725c671d76144b91e
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-08 01:16:45 +0600

    second commit

--- arguments.js
+++ arguments.js
@@ -1,6 +1,10 @@
 
 function sum(a,b,c,d){
     console.log(arguments)
+    console.log(arguments[4])
+    console.log(typeof arguments)
+    const args = [... arguments]
+    console.log(args)
     // arguments eta only function er mopdhhe pabo. baire pabo na
     const result = a+b+c+d
     return result
@@ -9,3 +13,5 @@ function sum(a,b,c,d){
 const total = sum(234, 345, 54, 76, 90,346, 8)
 // ei bracket er modhhe elements gula ke arguments bole 
 console.log(total)
+console.log(sum.length)
+// ei length diye koyta parameter ta bujhay
