7a94a7cb25d66ccbecee0f86ff0cdd7a52b20bd6
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-08 00:41:38 +0600

    second commit

--- clouser.js
+++ clouser.js
@@ -6,3 +6,23 @@ function kitchen () {
         return roast
     }
 }
+
+const firstServer = kitchen()
+console.log(firstServer())
+console.log(firstServer())
+console.log(firstServer())
+console.log(firstServer())
+console.log(firstServer())
+
+const firstServer2 = kitchen()
+console.log(firstServer2())
+console.log(firstServer2())
+
+
+console.log(firstServer())
+console.log(firstServer())
+console.log(firstServer2())
+console.log(firstServer())
+
+// clouser - nijer kache nijer man rakhe.. 
+// ekta function er vitore arekta function call korte hobe
